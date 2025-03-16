f8463a0fca078f7f6715a44281345746c297f51a
samirpramanik <samirpramanikjsr@gmail.com>
2025-03-16 18:32:46 +0400

    made div as flex to support RTL

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -3,18 +3,28 @@ import { InfiniteScrollList } from "./components/InfiniteScrollList";
 import { useTranslation } from "react-i18next";
 
 function App() {
-  const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
+  document.body.dir = i18n.dir();
 
   const changeLanguage = (lng: string) => {
+    console.log(i18n.dir());
     i18n.changeLanguage(lng, () => {
       console.log("locale changed to ", lng);
     });
+    document.body.dir = i18n.dir();
   };
 
   return (
     <div>
-      <button onClick={() => changeLanguage("en")}>english</button>
-      <button onClick={() => changeLanguage("ar")}>arabic</button>
+      <div id="localeSelector">
+        <button
+          style={{ marginInlineEnd: "10px" }}
+          onClick={() => changeLanguage("en")}
+        >
+          English
+        </button>
+        <button onClick={() => changeLanguage("ar")}>Arabic</button>
+      </div>
       <InfiniteScrollList />
     </div>
   );
