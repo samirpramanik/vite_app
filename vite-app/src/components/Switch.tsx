89b6fe9c52b5217bd02d2a25c786b4ba0289c823
samirpramanik <samirpramanikjsr@gmail.com>
2025-03-18 20:30:37 +0400

    new i18n text for change theme

--- vite-app/src/components/Switch.tsx
+++ vite-app/src/components/Switch.tsx
@@ -1,11 +1,14 @@
 import { useTheme } from "../hooks/useTheme";
+import { useTranslation } from "react-i18next";
 
 export const Switch = () => {
   const [theme, handleChange] = useTheme("light");
 
+  const { t } = useTranslation();
+
   return (
     <div className="container-switch">
-      <span>Change Theme </span>
+      <span>{t("themeLbl")}</span>
       <label className="switch">
         <input
           type="checkbox"
